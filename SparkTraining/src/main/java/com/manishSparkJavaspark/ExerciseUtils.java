cf091890cf18f3bbf4cfa07b09a73b6640c44a08
Pujya <33843556+Pujya@users.noreply.github.com>
2020-07-10 13:04:20 +0530

    refactor

--- SparkTraining/src/main/java/com/manishSparkJavaspark/ExerciseUtils.java
+++ SparkTraining/src/main/java/com/manishSparkJavaspark/ExerciseUtils.java
@@ -1,23 +1,26 @@
 package com.manishSparkJavaspark;
-
 import org.apache.spark.api.java.JavaRDD;
 
 public class ExerciseUtils {
-	
-public static void hold() {
-	while (true) {
-		try {
-			Thread.sleep(1000);
-		} catch(InterruptedException e) {
-			e.printStackTrace();
+
+	//A method to keep Java running, so that we can explore Spark instance.
+	public static void hold() {
+		while (true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
-}
-
-public static void printStringRDD(JavaRDD<String> StringRDD, int count) {
-	for (String s : StringRDD.take(count)) {
-		System.out.println(s);
+	
+	public static void printStringRDD(JavaRDD<String> stringRDD, int count) {
+		
+		for ( String s : stringRDD.take(count)) {
+			System.out.println(s);
+		}
+		System.out.println("-----------------------------------------------------------------------------------");
 	}
-	System.out.println("-----------------------------------------------------------------------------");
-}
+
 }
